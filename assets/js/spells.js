8b86b04c246f4c5038ca411ddf87c3bfcd337f44
Jonathan T <jhtran.dev@gmail.com>
2019-07-15 21:11:59 -0500

    small changes, weapons

--- assets/js/spells.js
+++ assets/js/spells.js
@@ -1,33 +1,35 @@
 var w;
 
 fetch("http://localhost:3008/spells", {
-    method: "GET",
-    mode: "cors",
-    headers: {
-        'Content-Type': 'application/json',
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-}).then(res =>{
-    return res.json()
-}).then(spells =>{
+  method: "GET",
+  mode: "cors",
+  headers: {
+    "Content-Type": "application/json"
+    // 'Content-Type': 'application/x-www-form-urlencoded',
+  }
+})
+  .then(res => {
+    return res.json();
+  })
+  .then(spells => {
     displaySpells(spells);
     s = spells;
-}).catch(err =>{
+  })
+  .catch(err => {
     return console.log(err);
-})
+  });
 
+let displaySpells = spells => {
+  spells.forEach(spell => {
+    let spellNode = document.createElement("div");
+    spellNode.className = "spell-card";
+    document.getElementById("spell-body").appendChild(spellNode);
+    let spellTemplate =
+      `<header> ` +
+      `<h1> ${spell.name} </h1> </header>` +
+      `<p> ${spell.spell_type} </p>` +
+      `<h3>${spell.slots}</h3>`;
 
-let displaySpells = (spells) =>{
-    spells.forEach(spell => {
-        let spellNode = document.createElement('div');
-        spellNode.className = 'spell-card';
-        document.getElementById('spell-body').appendChild(spellNode);
-        let spellTemplate = `<header> ` + `<h1> ${spell.name} </h1> </header>`
-        + `<p> ${spell.spell_type} </p>` + `<h3>${spell.slots}</h3>`;
-
-        spellNode.insertAdjacentHTML('beforeend', spellTemplate)
-    })
-
-}
-
-
+    spellNode.insertAdjacentHTML("beforeend", spellTemplate);
+  });
+};
