6e00c79e4c8295e0fcc14cef1a444adea02bab1c
Jonathan T <jhtran.dev@gmail.com>
2019-07-15 21:34:35 -0500

    made spell type capitalized no matter response, small boxes for framing, spacing

--- assets/js/spells.js
+++ assets/js/spells.js
@@ -19,6 +19,7 @@ fetch("http://localhost:3008/spells", {
     return console.log(err);
   });
 
+
 let displaySpells = spells => {
   spells.forEach(spell => {
     let spellNode = document.createElement("div");
@@ -26,10 +27,12 @@ let displaySpells = spells => {
     document.getElementById("spell-body").appendChild(spellNode);
     let spellTemplate =
       `<header> ` +
-      `<h1> ${spell.name} </h1> </header>` +
+      `<h1 >${spell.name} </h1> </header>` +
       `<p> ${spell.spell_type} </p>` +
-      `<h3>${spell.slots}</h3>`;
+      `<h3> Slots: ${spell.slots}</h3>`;
 
     spellNode.insertAdjacentHTML("beforeend", spellTemplate);
   });
 };
+
+
