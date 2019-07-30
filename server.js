e7956740cbda48b723451dc19a93dcfa9ab1d47f
patrickj188@gmail.com <Patrickj188@gmail.com>
2019-07-30 13:28:32 -0500

    fixed server issue

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const cors = require('cors')
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+
+// app.use('/', require('./app/routes/apiRoutes'));
 require("./app/routes/apiRoutes")(app);
 
 app.listen(PORT);
